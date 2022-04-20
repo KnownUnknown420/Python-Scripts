eda179212a26cc03ed7cc58b0f28c20889496907
Unknown_69 <88512161+KnownUnknown420@users.noreply.github.com>
2022-04-19 20:14:23 -0700

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,23 +1,33 @@
 #python password generator
+from ast import Pass
 import random
 import time
 
+PasswordAdd = 1
 PasswordLength = 5
 AllowedSymbols = (1,2,3,4,5,6,7,8,9,10,"q","w","e","r","t","y","u","i","o",
 "p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m","!","@",
 "#","$","%","^","&","*","(",")")
 
 #funtions
-
+def UpperCase(letter):
+    global PasswordAdd
+    Value = random.randint(0,1)
+    if Value == 0:
+        PasswordAdd = letter.upper()
 
 def intPassword():
     global AllowedSymbols
     global PasswordLength
+    global PasswordAdd
     PasswordFinal = ""
     print("Generating Password...:")
     for _ in range(PasswordLength):
         NumberIndex = random.randint(0,len(AllowedSymbols)-1)
-        PasswordFinal = PasswordFinal + str(AllowedSymbols[NumberIndex])
+        PasswordAdd = AllowedSymbols[NumberIndex]
+        if type(PasswordAdd) == str:
+            UpperCase(PasswordAdd)
+        PasswordFinal = PasswordFinal + str(PasswordAdd)
     print("Password Generated!")
     print(PasswordFinal)
     time.sleep(3)
@@ -25,9 +35,9 @@ def intPassword():
 def intSettings():
     global PasswordLength
     while True:
-        UserInput = input("---Settings---\n1: Password Length (" +str(PasswordLength) +")\n2: Back to menu")
+        UserInput = input("---Settings---\n1: Password Length (" +str(PasswordLength) +")\n2: Back to menu\n")
         if UserInput == str(1):
-            UserInput2 = input("Length of password: ")
+            UserInput2 = input("Length of password:\n")
             PasswordLength = int(UserInput2)
         elif UserInput == str(2):
             intMain()
