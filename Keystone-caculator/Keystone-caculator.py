e5c2ab47911506459b48b96f339c4ecc83001ec0
Unknown_69 <88512161+KnownUnknown420@users.noreply.github.com>
2022-04-20 22:22:07 -0700

    Update Keystone-caculator.py

--- Keystone-caculator/Keystone-caculator.py
+++ Keystone-caculator/Keystone-caculator.py
@@ -3,23 +3,32 @@
 #If odd, Multiply number by 3 and add 1
 #More Info here: https://www.youtube.com/watch?v=094y1Z2wpJg
 ActiveNumber = 1
+Output = False
 def Main():
+    global Output
     global ActiveNumber
     MathNumber = ActiveNumber
     KeyStoneCount = 0
+    print("Running Process...")
     while True:
         if MathNumber % 2 == 0:
             MathNumber = MathNumber / 2
         else:
             MathNumber = MathNumber * 3 + 1
-        print(MathNumber)
+        if Output:
+            print(MathNumber)
         KeyStoneCount = KeyStoneCount + 1
         if MathNumber == 1:
-            print("Loop Found!\nRaising ActiveNumber by 1...")
-            print(f"Total Steps: {KeyStoneCount}")
             ActiveNumber = ActiveNumber + 1
-            print(f"Active number: {ActiveNumber}")
-            print("Restarting loop...\n---")
+            if Output:
+                print("Loop Found!\nRaising ActiveNumber by 1...")
+                print(f"Total Steps: {KeyStoneCount}")
+                print(f"Active number: {ActiveNumber}")
+                print("Restarting loop...\n---")
+            ActiveNumber = ActiveNumber + 1
+            File = open("Keystone History.txt", "a")
+            File.write(f"\n---\nActive Number: {ActiveNumber}\nKeystones: {KeyStoneCount}\n---\n")
+            File.close()
             MathNumber = ActiveNumber
             KeyStoneCount = 0
             #TODO: Add a file system that saves total amount of keystones
